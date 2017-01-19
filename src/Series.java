e427d3347a5695dcef434930e8b4d46cea3df93b
manoranjanaDev <manoranjana.dev@gmail.com>
2017-01-19 10:18:35 +0530

    added a program  of series

---
+++ src/Series.java
@@ -0,0 +1,21 @@
+import java .util.*;
+public class Series
+{
+  public static void main(String args[])
+  {
+      Scanner in= new Scanner(System.in);
+      int n=in.nextInt();
+      double s=1.0;
+      int i;
+      double p=1.0;
+      double S=0.0; 
+      for(i=2;i<=n;i++)
+      {
+          s=s+i;
+          p=p*i;
+          S=s/p;
+        }
+        System.out.print(S);
+    }
+}
+     
