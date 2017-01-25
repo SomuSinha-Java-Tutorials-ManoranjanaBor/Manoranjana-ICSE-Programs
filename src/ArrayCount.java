73f23eebd7103b6d3a6344ccd1793ecfbde67718
manoranjanaDev <manoranjana.dev@gmail.com>
2017-01-25 21:07:36 +0530

    ADDED A PROGRAM OF ARRAY

--- src/ArrayCount.java
+++ src/ArrayCount.java
@@ -8,74 +8,63 @@
 import java.util.*;
 public class ArrayCount
 {
+	public static int countDigit(int a)
+	{
+		int c=0;
+		while(a>0)
+		{
+			a=a/10;
+			c++;
+		}
+	return c;
+	}
    public static void main(String args[])
    {
 	   Scanner in = new Scanner(System.in);
 	   int m[] = new int[10];
-	   int c=0;
-	   int n;
-	   int i;
-	   int tde=0,tdo=0,s=0,ps=1;
-	  
+	   int i,tdo=0,tde=0,s=0,ps=1;
+	   
 	   for(i=0;i<10;i++)
-	   {
-		   System.out.print("ENTER "+i+ " VALUES IN ARRAY :");
-		 m[i] = in.nextInt();   
+	   {   
+		   System.out.print("ENTER "+i+"TH THE VALUES : ");
+		   m[i]=in.nextInt();
 	   }
-	   
 	   for(i=0;i<10;i++)
 	   {
-		 while(m[i]!=0)
-		 {
-		  n=m[i]/10;
-		   c=0;
-		  c++;
-		  n++;
-		 }
-           if(c==2)
-           {
-          	 if((m[i]%2)==0)
-          	 {
-          		 tde++;
-          	 }
-           }
-	   
-	 while(m[i]!=0)
-		 {
-		  n=m[i]/10;
-          c=0;
-		  c++;
-		  n++;
-		 }
-           if(c==3)
-           {
-          	 if((m[i]%2)!=0)
-          	 {
-          		 tdo++;
-          	 }
-           }
-	   
+		   if(countDigit(m[i])==2)
+		   {
+			   if((m[i]%2)==0)
+			   {
+				   tde++;
+			   }
+		   }
+	   }
+	   for(i=0;i<10;i++)
+	   {
+		   if(countDigit(m[i])==3)
+		   {
+			   	if((m[i]%2)!=0)
+			   	{
+			   		tdo++;
+			   	}
+		   }
+	   }
+	   for(i=0;i<10;i++)
+	   {
 		   if(m[i]>50)
 		   {
-		     if((m[i]%2)==0)
-          	 {
-          		 s=s+m[i];
-          	 }
+			   s=s+m[i];
 		   }
-       
-	   
-		 while(m[i]!=0)
-		 {
-		  n=m[i]/10;
-		   c=0;
-		  c++;
-		  n++;
-		 }
-           if(c==1)
-           {
-        	   ps=ps*m[i];
-           }
-       }
+	   }
+	   for(i=0;i<10;i++)
+	   {
+		   if(countDigit(m[i])==1)
+		   {
+			  ps=ps*m[i];
+			  
+		   }
+	   }
+		   
 	   System.out.println("number of two digit even:"+tde);
 	   System.out.println("number of three digit odd:"+tdo);
 	   System.out.println("sum of all even above 50:"+s);
