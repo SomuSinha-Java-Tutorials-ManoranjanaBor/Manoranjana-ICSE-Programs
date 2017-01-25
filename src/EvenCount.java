6088d5c4eaa9335c87542a6904bd8cc018642b0c
manoranjanaDev <manoranjana.dev@gmail.com>
2017-01-25 17:51:51 +0530

    added a program to count even digits

---
+++ src/EvenCount.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class EvenCount
+{
+	public static int evenCount(int n)
+	{   int flag = 0 ;
+		int d;
+	
+	    while(n!=0)
+	    { 	
+	    	d = n % 10 ;
+	    	if((d % 2) == 0)
+	    	{
+	    		flag = 1 ;
+	    	}
+		n=n/10;
+	    }
+		return flag;
+	}
+	public static void main(String args[])
+	{ 
+		Scanner in = new Scanner(System.in);
+		System.out.println("ENTER THE NUMBER : ");
+		int n = in.nextInt();
+		int e;int ec=0;
+		
+		while(n!=0)
+		{
+			e = n % 10 ;
+			if(evenCount(e) == 1 )
+			{
+				ec++;
+			}
+			e=n/10;
+		}
+		System.out.println("number of even digits found : "+ec );
+	}
+}
