2bf04f4d2512ddb8f8acb1c76d8ae6032dc631c3
manoranjanaDev <manoranjana.dev@gmail.com>
2017-01-26 15:07:56 +0530

    Added a program to check prime numbers

--- src/PrimeCheck.java
+++ src/PrimeCheck.java
@@ -1,8 +1,44 @@
 import java.util.*;
 public class PrimeCheck 
 {
+	int m,i,flag;
 	PrimeCheck (int n)
 	{
-		
+		m=n;
+		flag=0;
+	}
+	void getVal()
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a number to check whether it is prime or not : ");
+		m=in.nextInt();
+	}
+	void checking()
+	{
+		for(i=2;i<=(m-1);i++)
+		{
+			if((m % i)==0)
+			{
+				flag = 1;
+			}
+		}
+	}
+	void display()
+	{
+		if(flag==1)
+		{
+			System.out.println("the number is not prime");
+		}
+		else
+		{
+			System.out.println("the number is prime");
+		}
+	}
+	public static void main(String args[])
+	{
+		PrimeCheck num = new PrimeCheck(0); 
+		num.getVal();
+		num.checking();
+		num.display();
 	}
 }
