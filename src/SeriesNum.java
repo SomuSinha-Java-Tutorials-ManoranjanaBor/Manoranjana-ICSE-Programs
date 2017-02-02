a6e5a6df577c2361b08abdf217d4fcaaff1cab35
manoranjanaDev <manoranjana.dev@gmail.com>
2017-02-02 18:10:55 +0530

    added a program of pattern

---
+++ src/SeriesNum.java
@@ -0,0 +1,26 @@
+/* 
+ * 1 3 5 7 9 
+ * 3 5 7 9 1
+ * 5 7 9 1 3 
+ * 7 9 1 3 5 
+ * 9 1 3 5 7 
+ */
+public class SeriesNum 
+{
+	public static void main (String args[])
+	{
+		int i,j,k;
+		for(i=1;i<=10;i+=2)
+		{
+			for(j=i;j<=10;j+=2)
+			{
+				System.out.print(j+" ");
+			}
+			for(k=1;k<=i-2;k+=2)
+			{
+				System.out.print(k+" ");
+			}
+			System.out.println();
+		}
+	}
+}
