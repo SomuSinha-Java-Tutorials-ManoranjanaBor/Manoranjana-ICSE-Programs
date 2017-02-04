fc26528dc71cfdb46fbdc6a86def3c5ffafb287d
manoranjanaDev <manoranjana.dev@gmail.com>
2017-02-04 18:34:56 +0530

    Added Maximum Prime program

---
+++ src/MaximumPrime.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class MaximumPrime 
+{
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner (System.in);
+		System.out.print("ENTER A NUMBER : ");
+		int n = in.nextInt();
+		int maxp=0;
+		int i,d;
+		while(n>0)
+		{
+			d = n % 10 ;
+			
+		}
+	}
+}
