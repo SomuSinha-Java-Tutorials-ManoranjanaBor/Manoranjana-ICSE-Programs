8d4195b7d5f5ff0e8cdd8849700b25ebbc185afd
Somenath Sinha <somenath.article@gmail.com>
2017-02-10 11:55:50 +0530

    Rectified Program for Series 2

--- src/Series2.java
+++ src/Series2.java
@@ -8,20 +8,19 @@ public class Series2
 {
 	public static void main(String args[])
 	{
-		int i,j,k=0;
+		int i,j,k=1;
 		for(i = 5 ; i >= 1 ; i --)
 		{
 			for(j = 1 ; j <= i; j++)
 			{
-				System.out.print(j);
+				if(k>9)
+					System.out.print(k+" ");
+				else
+					System.out.print(k+"  ");
+				k++;
 			}
 			
-			for(k = (i+j);k<=5;k++)
-				{
-					System.out.print(k);
-				}
-			
-					System.out.println();
+			System.out.println();
 		}
 	}
 }
