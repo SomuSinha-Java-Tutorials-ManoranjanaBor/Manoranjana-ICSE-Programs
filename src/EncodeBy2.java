693451174c7f49dca74122a48f953b76a22dd918
Somenath Sinha <somenath.article@gmail.com>
2017-02-10 12:26:10 +0530

    Rectified Program

--- src/EncodeBy2.java
+++ src/EncodeBy2.java
@@ -6,7 +6,7 @@ public class EncodeBy2
 		Scanner in = new Scanner (System.in) ;
 		System.out.print("ENTER A WORD TO GET IT ENCODED BY +2 : ");
 		String st= in.nextLine(),st1 = "";
-		int i,p=st.length(),n,x;
+		int i,p=st.length(),n,x=0;
 		char ch,ch1;
 		
 		for(i=0; i < p; i++)
@@ -15,7 +15,19 @@ public class EncodeBy2
 			if(ch != ' ')
 			{
 				n = (int) ch ;
-				x = n + 2 ;
+				if(n>=65&&n<=90)
+					x = 65+ (n-65+2)%26;
+				else if(n>=97&&n<=122)
+					x = 97+ (n-97+2)%26;
+				
+				/**
+				 * Character 	:	A	B	C	D	..	X	Y	Z
+				 * Index 		:	0	1	2	3	..	23	24	25
+				 * 
+				 * n-65 gives us an index in the series of A-Z. We add +2 for the encoding. This value mod 26 gives us the actual letter we need, which we then 
+				 * add to 65 to get the character from the index.
+				 */
+				
 				ch1 = (char) x ;
 				st1 = st1 + ch1 ;
 			}
