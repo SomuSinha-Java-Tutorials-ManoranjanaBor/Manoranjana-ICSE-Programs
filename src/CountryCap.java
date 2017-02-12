a159eb5d316af8155bcf80b57102312fa15a238c
manoranjanaDev <manoranjana.dev@gmail.com>
2017-02-12 13:20:15 +0530

    ractified errors

--- src/CountryCap.java
+++ src/CountryCap.java
@@ -4,28 +4,28 @@ public class CountryCap
 	public static void main(String args [])
 	{
 		Scanner in = new Scanner (System.in);
-		String coun[] = new String[2];
-		String cap[] = new String[2];
-		int i,k = 1,a=0;
-		for(i = 0 ; i < 2 ; i ++ )
+		String coun[] = new String[10];
+		String cap[] = new String[10];
+		int i,k = 1,a = 0 ;
+		for(i = 0 ; i < 10 ; i ++ )
 		{
 			System.out.print("ENTER THE NAME OF COUNTRY : ");
-			coun[i]=in.nextLine();
+			coun[i] = in.nextLine() ;
 			System.out.print("ENTER THE NAME OF CAPITAL : ");
-			cap[i]=in.nextLine();
+			cap[i] = in.nextLine() ;
 		}
-		System.out.print("ENTER THE NAME OF COUNTRY : ");
-		String c = in.nextLine();
+		System.out.print("\t ENTER THE NAME OF COUNTRY : ");
+		String c = in.nextLine() ;
 		
-		for(i = 0 ; i < 2 ; i ++ )
+		for(i = 0 ; i < 10 ; i ++ )
 		{
 			if(coun[i].equalsIgnoreCase(c))
 			{
-				k++;
-				a=i;
+				k ++ ;
+				a = i ;
 			}
 		}
-			if(k==2)
+			if(k == 2)
 			{
 				System.out.print(" THE NAME OF THE RESPECTIVE CAPITAL IS : "+cap[a]);
 			}
