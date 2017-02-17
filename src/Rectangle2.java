222356b8109b10ddeb43fddc77e542e9dc5c5314
manoranjanaDev <manoranjana.dev@gmail.com>
2017-02-17 14:20:07 +0530

    added a program of pattern

---
+++ src/Rectangle2.java
@@ -0,0 +1,22 @@
+/* OO
+ * OO
+ * OO
+ * OO
+ * OO
+ * 
+ */
+public class Rectangle2 
+{
+	public static void main(String args [])
+	{
+		int i , j ;
+		for(i=1;i<=5;i++)
+		{
+			for(j=1;j<=2;j++)
+			{
+				System.out.print("O");
+			}
+			System.out.println();
+		}
+	}
+}
