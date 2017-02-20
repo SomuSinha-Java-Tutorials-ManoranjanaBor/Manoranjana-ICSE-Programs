6164ee12c0586124a2d253f6fa7f75b2355129ff
manoranjanaDev <manoranjana.dev@gmail.com>
2017-02-20 19:13:27 +0530

    ADDED A PROGRAM

---
+++ src/MarksOfStudents.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MarksOfStudents 
+{
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner (System.in);
+		int n,i,m1[],m2[],m3[];
+		String name[];
+		double avg = 0.0 ;
+		
+	}
+}
