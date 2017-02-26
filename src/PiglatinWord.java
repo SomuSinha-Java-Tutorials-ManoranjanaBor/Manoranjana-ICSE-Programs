dfab7fccc5116a6ba369b60d6a8f74a8e37b16e5
manoranjanaDev <manoranjana.dev@gmail.com>
2017-02-26 18:55:04 +0530

    RECTIFIED THE ERRORS IN THE PROGRAM

--- src/PiglatinWord.java
+++ src/PiglatinWord.java
@@ -10,11 +10,11 @@ public class PiglatinWord
 	public static void main(String args [])
 	{
 		Scanner in = new Scanner (System.in); 
-		System.out.print("ENTER THE WORD TO GET THE WORD CHANGED INTO PIGLATIN : ");
+		System.out.print("ENTER THE WORD in UPPERCASE LETTER TO GET THE WORD CHANGED INTO PIGLATIN : ");
 		String s = in.nextLine();
 		String s1 = "" ;
 		String s2 = "" ;
-		int i , p = s.length() ;
+		int i , p = s.length() , pos = 0 ;
 		char ch ,ch1;
 		
 		for(i = 0 ; i < p ; i ++)
@@ -24,31 +24,18 @@ public class PiglatinWord
 			{
 				if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
 				{
-					s1 += s.substring(i , (p-1));
-					System.out.println(s1);
+					pos = i ;
+					s1 += s.substring(pos , (p));
+					break;
+				}
+				else
+				{ 
 					
-				}
-				else
-				{
-					s2 += ch ; 
-				}
-				System.out.println("THE PIGLATIN FORM OF WORD IS : "+s1+s2+"ay");
-			}
-			if(Character.isLowerCase(ch))
-			{
-				ch1 = Character.toUpperCase(ch);
-				
-				if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
-				{
-					s1 += s.substring(i , (p-1));
-					break ;
-				}
-				else
-				{
-					s2 += ch ; 
+					s2 = s.substring(0, (i+1)) ; 
 				}
 			}
 		}
+		System.out.println("THE PIGLATIN FORM OF WORD IS : "+s1+s2+"AY");
 	}
 }
-}
+
