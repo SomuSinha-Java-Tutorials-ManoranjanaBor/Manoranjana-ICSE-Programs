50374c8865fe607e44b60b84372ab8aadb8439e6
manoranjanaDev <manoranjana.dev@gmail.com>
2017-03-03 18:27:19 +0530

    added a program to print sum of till 100 prime numbers

--- src/SumPrime.java
+++ src/SumPrime.java
@@ -3,6 +3,35 @@ Write a program to calculate the sum of all the prime numbers between the range 
 1 and 100. 
  * 
  */
-public class SumPrime {
-
+import java.util.*;
+public class SumPrime
+{
+	public static int prime(int a)
+	{
+		int flag = 0 , j ;
+		for( j = 1 ; j <= a ; j ++)
+		{
+			if( ( a % j ) == 0 )
+			{
+				flag = 1 ; 
+			}
+		}
+		return flag ;
+	}
+	
+	public static void main(String args [])
+	{
+		Scanner in = new Scanner (System.in);
+		System.out.print("ENTER THE RANGE AS 100 : ");
+		int n = in.nextInt();
+		int i , sum = 0 ;
+		for( i = 1 ; i <= n ; i ++)
+		{
+			if(prime(i) == 0 )
+			{
+				sum = sum + i ;
+			}
+		}
+		System.out.println("THE SUM OF THE PRIME NUMBER BETWEEN 1 TO 100 : " + sum);
+	}
 }
